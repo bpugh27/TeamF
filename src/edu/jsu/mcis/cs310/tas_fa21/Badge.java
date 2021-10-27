28467533d829b5233d9362f91ff99f5a63627337
Brandon Pugh <brandonpugh20@gmail.com>
2021-10-27 12:52:46 -0500

    Updated Badge. testGetPunches and testGetBadges should pass

--- src/edu/jsu/mcis/cs310/tas_fa21/Badge.java
+++ src/edu/jsu/mcis/cs310/tas_fa21/Badge.java
@@ -31,5 +31,16 @@ public class Badge {
         this.description = description;
     }
     
+    public String toString() {
+        
+        // #12565C60 (Chapman, Joshua E)
+        
+        StringBuilder s = new StringBuilder();
+        
+        s.append('#').append(id).append(" (").append(description).append(')');
+        
+        return s.toString();
+        
+    }
     
 }
