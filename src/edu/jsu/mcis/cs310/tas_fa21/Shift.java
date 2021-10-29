ff44e3d37930896b9cccbf2270430f38c6feef10
Brandon Pugh <brandonpugh20@gmail.com>
2021-10-29 11:47:17 -0500

    Updated shift

--- src/edu/jsu/mcis/cs310/tas_fa21/Shift.java
+++ src/edu/jsu/mcis/cs310/tas_fa21/Shift.java
@@ -1,27 +1,19 @@
-//<<<<<<< OURS
 package edu.jsu.mcis.cs310.tas_fa21;
 
 import java.util.Calendar;
 import java.sql.*;
 import java.text.SimpleDateFormat;
+import java.time.LocalTime;
 import java.util.GregorianCalendar;
-import java.time.*;
-import java.time.temporal.ChronoUnit;
 
 
 
 public class Shift {
-    private LocalTime shiftBegin, lunchBegin, lunchEnd, shiftEnd;
+    private Time shiftBegin, lunchBegin, lunchEnd, shiftEnd;
     private int id, gracePeriod, dock, interval, lunchDeduct;
     private final String description;
     
-    //Shift should read a timestamp as a long type
-    //then converted and stored as LocalTime or LocalDateTime variables
-    //Seconds and nanoseconds should be zeroed
-    
-    
-    public Shift(int interval, int id, int gracePeriod, LocalTime begin, int dock,
-            LocalTime lunchBegin, int lunchDeduct, LocalTime lunchEnd, LocalTime end, String description) {
+    public Shift(int interval, int id, int gracePeriod, Time begin, int dock, Time lunchBegin, int lunchDeduct, Time lunchEnd, Time end, String description) {
         this.interval = interval;
         this.id = id;
         this.gracePeriod = gracePeriod;
@@ -39,19 +31,23 @@ public class Shift {
     }
     //getter
 
-    public LocalTime getShiftBegin() {
+    Shift(int interval, int shiftID, int gracePeriod, LocalTime shiftStart, int dock, LocalTime lunchStart, int lunchDeduct, LocalTime lunchStop, LocalTime shiftStop, String description) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Time getShiftBegin() {
         return shiftBegin;
     }
 
-    public LocalTime getLunchBegin() {
+    public Time getLunchBegin() {
         return lunchBegin;
     }
 
-    public LocalTime getLunchEnd() {
+    public Time getLunchEnd() {
         return lunchEnd;
     }
 
-    public LocalTime getShiftEnd() {
+    public Time getShiftEnd() {
         return shiftEnd;
     }
 
@@ -79,45 +75,33 @@ public class Shift {
         return description;
     }
     
-    
-//    private long getElapsedTime(Time s, Time e) {
-//        
-//        Calendar BeginCal = GregorianCalendar.getInstance();
-//        Calendar endCal = GregorianCalendar.getInstance();
-//        BeginCal.setTimeInMillis(s.getTime());
-//        endCal.setTimeInMillis(e.getTime());
-//        long begin, end;
-//        begin = BeginCal.getTimeInMillis();
-//        end = endCal.getTimeInMillis();
-//        
-//        return (end - begin) / (60 * 1000);
-//    }   
-    
+    private long getElapsedTime(Time s, Time e) {
+        Calendar BeginCal = GregorianCalendar.getInstance();
+        Calendar endCal = GregorianCalendar.getInstance();
+        BeginCal.setTimeInMillis(s.getTime());
+        endCal.setTimeInMillis(e.getTime());
+        long begin, end;
+        begin = BeginCal.getTimeInMillis();
+        end = endCal.getTimeInMillis();
+        return (end - begin) / (60 * 1000);
+    }
     @Override
     public String toString() {
     
-        String beginTime = shiftBegin.toString();
-        String lunchBeginTime = lunchBegin.toString();
-        String lunchEndTime = lunchEnd.toString();
-        String endTime = shiftEnd.toString();
+        String beginTime = (new SimpleDateFormat("HH:mm")).format(shiftBegin.getTime());
+        String lunchBeginTime = (new SimpleDateFormat("HH:mm")).format(lunchBegin.getTime());
+        String lunchEndTime = (new SimpleDateFormat("HH:mm")).format(lunchEnd.getTime());
+        String endTime = (new SimpleDateFormat("HH:mm")).format(shiftEnd.getTime());
+        String data = "";
         
-//        data += description + ": ";
-//        data += beginTime + " - ";
-//        data += getElapsedTime(shiftBegin, shiftEnd) + " minutes);";
-//        data += " Lunch: " + lunchBeginTime + " - ";
-//        data += lunchEndTime + " (";
-//        data += getElapsedTime(lunchBegin, lunchEnd) + " minutes)";
-//        data += endTime + " (";
-        
-        //        assertEquals("Shift 1: 07:00 - 15:30 (510 minutes); Lunch: 12:00 - 12:30 (30 minutes)", s1.toString());
-        //"Shift 1: 07:00 - 15:30 (510 minutes); Lunch: 12:00 - 12:30 (30 minutes)",
-        StringBuilder d = new StringBuilder();
-        d.append(description).append(": ").append(beginTime).append(" - ").append(endTime).append(" (");
-        d.append(shiftBegin.until(shiftEnd, ChronoUnit.MINUTES)).append(" minutes); ");
-        d.append("Lunch: ").append(lunchBeginTime).append(" - ").append(lunchEndTime).append(" (").append(lunchBegin.until(lunchEnd, ChronoUnit.MINUTES));
-        d.append(" minutes)");
-        
-        
-        return d.toString();
+        data += description + ": ";
+        data += beginTime + " - ";
+        data += getElapsedTime(shiftBegin, shiftEnd) + " minutes);";
+        data += " Lunch: " + lunchBeginTime + " - ";
+        data += lunchEndTime + " (";
+        data += getElapsedTime(lunchBegin, lunchEnd) + " minutes)";
+        data += endTime + " (";
+        return data;
     }
+      
 }
